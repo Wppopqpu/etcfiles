47859debd16090bffadadcc48b948558a51b3ee1
Wppopqpu <wppopqpu@outlook.com>
2025-03-08 21:11:17 +0800

    feat: update update_etc.fish add colored output enable fetching from target

--- update_etc.fish
+++ update_etc.fish
@@ -3,14 +3,24 @@
 set -l options (fish_opt --short h --long help)
 set -la options (fish_opt --short p --long pretend)
 set -la options (fish_opt --short a --long all)
+set -la options (fish_opt --short r --long fetch)
 
 argparse $options -- $argv
+
+function colored_critical
+	echo -e "\033[30m\033[45m$argv\033[0m"
+end
+
+function echo_critical
+	echo (colored_critical $argv)
+end
 
 if set -q _flag_help
 	echo -e "Options:"
 	echo -e "\t-h,\t--help:\tprint help message;"
 	echo -e "\t-p,\t--pretend:\tdo nothing;"
 	echo -e "\t-a,\t--all:\tforce update all;"
+	echo -e "\t-r,\t--fetch:\tfetch from target;"
 
 	echo -e "\nWARNING: need to be runned in stow dir."
 
@@ -18,10 +28,11 @@ if set -q _flag_help
 end
 if set -q _flag_pretend
 	function execute
-		echo $argv
+		echo \t\t $argv
 	end
 else
 	function execute
+		echo -e -n "\t\t"
 		echo $argv | tee /dev/tty | source
 	end
 end
@@ -43,15 +54,39 @@ else
 	set update_all false
 end
 
+if set -q _flag_fetch
+	set fetch_from_target true
+else
+	set fetch_from_target false
+end
+
 function install
-	if test -f argv[2] && test (stat -c "%Y" $argv[1]) -le (stat -c "%Y" $argv[2]) -a false = $update_all
-		return
+	echo -e -n "\t$(colored_critical INSTALLING): $argv[1] -> $argv[2] \t;"
+	if test -f $argv[2]
+		set source_timestamp (stat -c "%Y" $argv[1])
+		set target_timestamp (stat -c "%Y" $argv[2])
+		if test $source_timestamp -eq $target_timestamp -a false = $update_all
+			echo_critical "SKIPPED"
+			return
+		end
+		if test $source_timestamp -lt $target_timestamp -a true = $fetch_from_target
+			echo_critical "FETCHED"
+			# if test -e $argv[1]
+			# 	execute rm $argv[1]
+			# end
+			execute cp $argv[2] $argv[1]
+			execute touch --reference=$argv[2] $argv[1]
+			execute chown branch $argv[1]
+			return
+		end
 	end
+	echo_critical "UPDATED"
 	if test -e $argv[2]
 		# execute mv $argv[1] $TMP_DIR
 		execute rm $argv[2]
 	end
 	execute cp  $argv[1] $argv[2]
+	execute touch --reference=$argv[1] $argv[2]
 	execute chown root $argv[2]
 end
 
@@ -86,7 +121,8 @@ function update_etc
 			continue
 		end
 
-		execute cd $p
+		cd $p
+		echo (colored_critical PACKAGE): $p
 		for i in **
 			if test -d $i
 				clear_dir /$i
@@ -94,7 +130,7 @@ function update_etc
 				install $i /$i
 			end
 		end
-		execute cd ..
+		cd ..
 	end
 end
 
